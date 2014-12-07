b0d9f110e98225499b17a0cedc59b50adb48670f
Yassin <yassin791@gmail.com>
2014-12-07 17:03:20 -0200

    remove mopub sdk

--- mopub-sdk/src/main/java/com/mopub/common/event/EventRecorder.java
+++
@@ -1,9 +0,0 @@
-package com.mopub.common.event;
-
-/**
- * This interface represents a backend to which MoPub client events are logged.
- */
-public interface EventRecorder {
-    public void recordEvent(Event event);
-    public void recordTimedEvent(TimedEvent event);
-}
