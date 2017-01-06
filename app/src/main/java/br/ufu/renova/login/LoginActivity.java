7a0554cd77e8f355e1ca9a342dc1ba37c6503322
Yassin <yassin791@gmail.com>
2017-01-06 16:12:58 +0200

    Subindo targetSdk e baixando minSdk, é possivel abrangir mais versões. Fix compatibilidade dos estilos.

--- app/src/main/java/br/ufu/renova/login/LoginActivity.java
+++ app/src/main/java/br/ufu/renova/login/LoginActivity.java
@@ -1,10 +1,10 @@
 package br.ufu.renova.login;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +17,7 @@ import br.ufu.renova.preferences.PreferencesContract;
 import br.ufu.renova.scraper.ILibraryDataSource;
 
 
-public class LoginActivity extends Activity implements LoginContract.View {
+public class LoginActivity extends AppCompatActivity implements LoginContract.View {
 
     private LoginContract.Presenter mPresenter;
 
--- app/src/main/java/br/ufu/renova/preferences/SettingsFragment.java
+++ app/src/main/java/br/ufu/renova/preferences/SettingsFragment.java
@@ -1,12 +1,12 @@
 package br.ufu.renova.preferences;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import br.ufu.renova.R;
